7711bdf3f1aa44dd95179a0877655e36d072c2c6
Kristen Abby Cendana <kristencendana@gmail.com>
2023-03-23 13:27:07 -0700

    ready for deployment

--- components/About.tsx
+++ components/About.tsx
@@ -42,11 +42,11 @@ function About({pageInfo}: Props) {
         }}
         viewport={{once: true}}
         src={urlFor(pageInfo?.profilePic).url()}
-        className='-mb-20 mt-[3rem] md:mb-0 flex-shrink-0 xxs:w-40 xxs:h-40 xs:w-40 xs:h-40 short:w-56 short:h-56 w-56 h-56 
-        xxs:rounded-full xs:rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px] xxs:pb-0 xs:pb-0 pb-5'
+        className='-mb-20 mt-[3rem] md:mb-0 flex-shrink-0 xxs:w-40 xxs:h-40 xs:w-40 xs:h-40 short:w-40 short:h-40 w-56 h-56 
+        xxs:rounded-full xs:rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[400px] xxs:pb-0 xs:pb-0 md:pt-5 xl:pt-5'
       />
       <div className='px-0 md:px-10 text-center'>
-        <h4 className='xxs:mt-8 xs:mt-8 xs:mb-0 xxs:text-xl xs:text-xl short:text-2xl text-4xl font-semibold'>
+        <h4 className='xxs:mt-8 xs:mt-8 xs:mb-0 xxs:text-xl xs:text-xl short:text-2xl mt-10 text-2xl font-semibold'>
           A <span className='underline decoration-[#E7E8D1]/50'>little</span> bit about me {text}</h4>
           <p className='xxs:text-[8px] xs:text-[10px] text-base text-[#E7E8D1]'>
             {pageInfo?.backgroundInformation} <Link href='#contact'><span className='decoration underline'>Contact Me!</span></Link>
